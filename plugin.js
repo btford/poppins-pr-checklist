faed4692709bcbe26a5c0e2ee3ee62c16845fc86
Brian Ford <btford@umich.edu>
2014-02-05 14:26:49 -0800

    feat: checks can return strings explaining why a check failed

--- plugin.js
+++ plugin.js
@@ -49,6 +49,17 @@ function responseBody (data) {
 function checklist (data) {
   return Q.all(prChecklist.checks.map(function (check) {
       return check.condition(data).then(function (condition) {
+        // if the condition is a string, it explains why the box is unchecked
+        // if it's an empty string, the box should be checked
+        // if it's bool, true = checked, false = unchecked
+        // yay backwards compatibility-driven APIs
+        if (typeof condition === 'string') {
+          if (condition.length === 0) {
+            condition = true;
+          } else {
+            return prChecklist.checkbox(false) + check.message + ' (' + condition + ')';
+          }
+        }
         return prChecklist.checkbox(condition) + check.message;
       });
     })).
